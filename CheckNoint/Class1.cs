b3624fa64b8e4b921293e3d734a19dd80be9fb2f
Pink <62712899+BSMGPink@users.noreply.github.com>
2020-11-02 23:22:09 +0000

    Add files via upload

---
+++ CheckNoint/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using MelonLoader;
+namespace CheckNoint
+{
+    public class CheckNoint : MelonMod
+    {
+
+        public override void OnApplicationStart()
+        {
+            MelonModLogger.Log("CHECKPOINT IS GONE");
+        }
+
+        [HarmonyPatch(typeof(TrackList), "ShouldBeFilteredInList")]
+        private static class CHECKPOINTISGONE
+        {
+            public static void Postfix(TrackList __instance, TrackInfoAssetReference trackInfoAssetRef, ref bool __result)
+            {
+                if (trackInfoAssetRef.UniqueName == "Checkpoint")
+                {
+                    __result =  false;
+                }
+            }
+        }
+    }
+}
